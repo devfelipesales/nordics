932ac33c954ccacd851ff4b01a587a4e1d06c051
Felipe Sales <felipefsp.dev@gmail.com>
2023-01-17 11:12:44 -0300

    criacao da secao de galeria

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 import MenuMobile from './module/menu-mobile.js';
+import SlideNav from './module/slide.js';
 
 // ------------------------------------------------------------------------
 // MENU MOBILE
 // ------------------------------------------------------------------------
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 menuMobile.init();
+
+// ----------------------------------------------------------------------------------------------
+// Slide
+// -----------------------------------------------------------------------------------------------
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+// slide.addArrow('.prev', '.next'); // sem navegação por arrow
+slide.addControl('.custom-controls');
